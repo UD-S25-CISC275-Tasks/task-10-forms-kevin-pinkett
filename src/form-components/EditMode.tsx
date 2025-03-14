eb43ab6acf5b34b85995e004448b7a6fa8b28ae0
Kevin Pinkett <kpinkett@udel.edu>
2025-03-14 18:12:34 -0400

    EditMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -6,49 +6,69 @@ export function EditMode(): React.JSX.Element {
     const [name, setName] = useState<string>("Your Name");
     const [isStudent, updateStudent] = useState<boolean>(true);
 
+    const [message, setMessage] = useState<string>(
+        name.concat(" is a student"),
+    );
+
+    function updateMessage() {
+        if (isStudent) {
+            setMessage(name.concat(" is a student"));
+        } else {
+            setMessage(name.concat(" is not a student"));
+        }
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
             <Form.Group controlId="formEditMode">
                 <Form.Check
                     type="switch"
-                    label="Edit Mode"
+                    id="enable-edit-mode"
+                    label="edit"
                     checked={editMode}
                     onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                         setMode(event.target.checked);
+                        updateMessage();
                     }}
                 />
-                {editMode && (
-                    <Form.Group>
-                    <Form.Label>Name: </Form.Label>
-                    <Form.Control
-                        disabled={!editMode}
-                        value={name}
-                        onChange={(
-                            event: React.ChangeEvent<HTMLInputElement>,
-                        ) => {
-                            setName(event.target.value);
-                        }}
-                    />
-                </Form.Group>)}
-                {editMode && (
-                    <Form.Check
-                        type="checkbox"
-                        label="Student?"
-                        checked={isStudent}
-                        onChange={(
-                            event: React.ChangeEvent<HTMLInputElement>,
-                        ) => {
-                            updateStudent(event.target.checked);
-                        }}
-                    />
-                )}
+                <span>
+                    {editMode && (
+                        <Form.Group>
+                            <Form.Label>Name: </Form.Label>
+                            <Form.Control
+                                value={name}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>,
+                                ) => {
+                                    setName(event.target.value);
+                                    updateMessage();
+                                }}
+                            />
+                        </Form.Group>
+                    )}
+                    {editMode && (
+                        <Form.Check
+                            type="checkbox"
+                            label="Student?"
+                            name="student"
+                            checked={isStudent}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>,
+                            ) => {
+                                updateStudent(event.target.checked);
+                                updateMessage();
+                            }}
+                        />
+                    )}
+                </span>
             </Form.Group>
+
             <span>
                 {!editMode && (
                     <span>
-                        {isStudent && <span>{name} is a student</span>}
-                        {!isStudent && <span>{name} is not a student</span>}
+                        {isStudent && <span>{message}</span>}
+                        {!isStudent && <span>{message}</span>}
                     </span>
                 )}
             </span>

34ea7bd3c46aa285c390ed30b2f142ec876394c9
Kevin Pinkett <kpinkett@udel.edu>
2025-03-14 20:38:28 -0400

    ChangeColor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+
+const COLORS = ["red","orange","yellow","green","blue","purple","gold","lightcoral"];
+const DEFAULT_COLOR = COLORS[0]
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>(DEFAULT_COLOR);
+
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Group controlId="changeColor">
+                {COLORS.map((c: string) => 
+                    <Form.Check
+                        inline
+                        type="radio"
+                        label={c}
+                        value={c}
+                        onChange={(e) => {setColor(e.target.value)}}
+                        key={c}
+                        checked={color === c}
+                    >
+
+                    </Form.Check>
+                )}
+                <span>You have chosen <span data-testid="colored-box" style={{
+                    backgroundColor: color
+                }}>{color}</span></span>
+            </Form.Group>
         </div>
     );
 }
